40299b95a8507317f3a2574f7fe97c108c156385
BSheetal001 <sheetalgupta000001@gmail.com>
2018-10-15 16:51:24 +0530

    initial commit

---
+++ app/src/main/java/com/example/android/customchrometabs/ChromeActivity.java
@@ -0,0 +1,48 @@
+package com.example.android.customchrometabs;
+
+import android.net.Uri;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChromeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chrome);
+
+        Button button1 = findViewById(R.id.github_button);
+        Button button2 = findViewById(R.id.webmail_button);
+
+        final List<String> url = new ArrayList<>();
+//        final String url1 = "https://github.com/BVShe001";
+//        final String url2 = "https://www.google.com";
+        url.add("https://github.com/BVShe001");
+        url.add("https://www.google.com");
+        url.add("https://ashoka.iitp.ac.in");
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        final CustomTabsIntent customTabsIntent = builder.build();
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customTabsIntent.launchUrl(ChromeActivity.this, Uri.parse(url.get(0)));
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customTabsIntent.launchUrl(ChromeActivity.this, Uri.parse(url.get(2)));
+
+            }
+        });
+    }
+}
