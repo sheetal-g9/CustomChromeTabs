03f25c5327841c6238c4e4504b35b30deb446256
femtioelva <nejfredrika@gmail.com>
2018-10-16 19:41:40 +0200

    add list of popular websites

--- app/src/main/java/com/example/android/customchrometabs/ChromeActivity.java
+++ app/src/main/java/com/example/android/customchrometabs/ChromeActivity.java
@@ -26,8 +26,40 @@ public class ChromeActivity extends AppCompatActivity {
 //        final String url2 = "https://www.google.com";
         url.add("https://github.com/BVShe001");
         url.add("https://www.google.com");
-        url.add("https://ashoka.iitp.ac.in");
-        url.add("https://172.16.1.6");
+        url.add("https://www.youtube.com");
+        url.add("https://www.wikipedia.org");
+        url.add("https://www.twitter.com");
+        url.add("https://www.instagram.com");
+        url.add("https://www.reddit.com");
+        url.add("https://www.medium.com");
+        url.add("https://www.netflix.com");
+        url.add("https://www.twitch.tv");
+        url.add("https://www.vimeo.com");
+        url.add("https://www.bandcamp.com");
+        url.add("https://www.yahoo.com");
+        url.add("https://www.espn.com");
+        url.add("https://www.cnn.com");
+        url.add("https://www.nytimes.com");
+        url.add("https://www.bbc.co.uk");
+        url.add("https://www.theguardian.com");
+        url.add("https://www.forbes.com");
+        url.add("https://time.com");
+        url.add("https://www.amazon.com");
+        url.add("https://www.ebay.com");
+        url.add("https://www.taobao.com");
+        url.add("https://www.etsy.com");
+        url.add("https://www.pinterest.com");
+        url.add("https://www.linkedin.com");
+        url.add("https://www.imdb.com");
+        url.add("https://www.dropbox.com");
+        url.add("https://www.imgur.com");
+        url.add("https://www.flickr.com");
+        url.add("https://www.ted.com");
+        url.add("https://www.techcrunch.com");
+        url.add("https://www.stackoverflow.com");
+        url.add("https://archive.org");
+        
+        
 
         CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
         final CustomTabsIntent customTabsIntent = builder.build();
