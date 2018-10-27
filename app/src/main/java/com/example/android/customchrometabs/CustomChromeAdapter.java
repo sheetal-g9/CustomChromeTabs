bd6028fd2425762c5eb652deed2e2f3d160e189f
BSheetal001 <sheetalgupta000001@gmail.com>
2018-10-27 06:11:33 +0530

    add recyclerView adapter and set on clicklisteners

---
+++ app/src/main/java/com/example/android/customchrometabs/CustomChromeAdapter.java
@@ -0,0 +1,57 @@
+package com.example.android.customchrometabs;
+
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class CustomChromeAdapter extends RecyclerView.Adapter<CustomChromeAdapter.UrlHolder> {
+    Context context;
+    List<Url> urls;
+
+    public CustomChromeAdapter(List<Url> urls) {
+        this.urls = urls;
+    }
+
+    @NonNull
+    @Override
+    public UrlHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        LayoutInflater layoutInflater = LayoutInflater.from(viewGroup.getContext());
+        return new UrlHolder(layoutInflater.inflate(R.layout.url_itemview,viewGroup,false));
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UrlHolder urlHolder, int i) {
+        urlHolder.getUrlTextView().setText(urls.get(i).getUrl());
+    }
+
+    @Override
+    public int getItemCount() {
+        return urls !=null?urls.size():0;
+    }
+
+    public class UrlHolder extends RecyclerView.ViewHolder {
+        TextView urlTextView;
+        public UrlHolder(@NonNull final View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ((ChromeActivity) v.getContext()).onUrlClick(urls.get(getAdapterPosition()));
+                }
+            });
+            urlTextView = itemView.findViewById(R.id.url_textView);
+        }
+
+        public TextView getUrlTextView() {
+            return urlTextView;
+        }
+    }
+}
