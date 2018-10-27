11a5f4deed10f2a74fcc523f6a9b3f81f34c107a
BSheetal001 <sheetalgupta000001@gmail.com>
2018-10-27 06:10:09 +0530

    add Url class to store urls.

---
+++ app/src/main/java/com/example/android/customchrometabs/Url.java
@@ -0,0 +1,20 @@
+package com.example.android.customchrometabs;
+
+import java.util.List;
+
+public class Url {
+    public Url(String url) {
+        this.url = url;
+    }
+
+    String url;
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+
+    public String getUrl() {
+        return url;
+    }
+}
